85a20bd5011eb1f8f1ad1b198d5a0952f3127b83
xiaoming <954531417@qq.com>
2019-07-05 14:18:50 +0800

    click

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -35,8 +35,7 @@ class IndexController extends Controller
      * @return array
      */
     public function info(){
-        $id = \Request::input('id');
-        dd($id);
+        $id = Request::input('id');
         $atricleModel =  new Article();
         $atricleModel->Click($id+0);
         $data['details'] = $atricleModel->Details($id);
